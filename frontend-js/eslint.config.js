0c428afb173cfd951c187e349557218fe2e1f681
Arshman Zahid <arshman@gmail.com>
2026-06-05 17:01:28 +0500

    fix: add jsx parser support to eslint config

--- frontend-js/eslint.config.js
+++ frontend-js/eslint.config.js
@@ -1,6 +1,16 @@
+import globals from "globals"
+
 export default [
   {
     files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
     rules: {},
   },
 ]
